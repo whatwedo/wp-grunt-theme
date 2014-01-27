a46c9795d14b37ded18db8da0d25604e942c0d7b
René Stalder <renestalder@me.com>
2014-01-27 12:04:44 +0100

    Clean out templates from old code

--- templates/front-page.php
+++ templates/front-page.php
@@ -4,63 +4,6 @@
  */
 get_header(); ?>
 
-<header class="header">
-<?php
-$post = get_field('post');
-if($post): /** ist ein Blog-Beitrag geteaserd? **/
-?>
-    <article class="post-teaser">
-        <h1><?php the_field('front-page-excerpt', $post->ID); ?></h1>
-        <a class="button" href="<?php the_permalink($post->ID); ?>">Mehr lesen</a>
-    </article>
-<?php endif; ?>
-</header>
-<div id="content">
-    <div class="page-intro">
-        <?php the_field("introduction"); ?>
-    </div>
-    <section class="products">
-        <h1>Produkte</h1>
-        <ul class="products-list">
-            <li>
-                <a href="#">
-                    <img src="http://placehold.it/350x150" alt="Produkt Bild"/>
-                    <h2>Bezeichnung</h2>
-                    <div class="product-price-preview">
-                        CHF 29.-
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="http://placehold.it/350x150" alt="Produkt Bild"/>
-                    <h2>Bezeichnung</h2>
-                    <div class="product-price-preview">
-                        CHF 29.-
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="http://placehold.it/350x150" alt="Produkt Bild"/>
-                    <h2>Bezeichnung</h2>
-                    <div class="product-price-preview">
-                        CHF 29.-
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="http://placehold.it/350x150" alt="Produkt Bild"/>
-                    <h2>Bezeichnung</h2>
-                    <div class="product-price-preview">
-                        CHF 29.-
-                    </div>
-                </a>
-            </li>
-        </ul>
-    </section>
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
     <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
